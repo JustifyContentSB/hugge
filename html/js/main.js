711b362fcdf9a43df6a3a221bcc9a003374c0791
RxPro <wxweb@mail.ru>
2021-12-16 16:17:15 +0300

    Верстка страницы галереи

--- html/js/main.js
+++ html/js/main.js
@@ -18,4 +18,121 @@ $(document).ready(function () {
       }
     });
   }
+
+  var gallerySwiper1 = new Swiper(".gallery-1 .gallery__swiper", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 500,
+    loop: false,
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 20
+      },
+      900: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 20
+      },
+      1200: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 20
+      }
+    },
+    navigation: {
+      prevEl: ".gallery-1 .gallery__prev",
+      nextEl: ".gallery-1 .gallery__next"
+    }
+  });
+  var gallerySwiper2 = new Swiper(".gallery-2 .gallery__swiper", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 500,
+    loop: false,
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 20
+      },
+      900: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 20
+      },
+      1200: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 20
+      }
+    },
+    navigation: {
+      prevEl: ".gallery-2 .gallery__prev",
+      nextEl: ".gallery-2 .gallery__next"
+    }
+  });
+  var gallerySwiper3 = new Swiper(".gallery-3 .gallery__swiper", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 500,
+    loop: false,
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 20
+      },
+      900: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 20
+      },
+      1200: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 20
+      }
+    },
+    navigation: {
+      prevEl: ".gallery-3 .gallery__prev",
+      nextEl: ".gallery-3 .gallery__next"
+    }
+  });
+  var gallerySwiper4 = new Swiper(".gallery-4 .gallery__swiper", {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
+    speed: 500,
+    loop: false,
+    breakpoints: {
+      600: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        spaceBetween: 20
+      },
+      900: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        spaceBetween: 20
+      },
+      1200: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 20
+      }
+    },
+    navigation: {
+      prevEl: ".gallery-4 .gallery__prev",
+      nextEl: ".gallery-4 .gallery__next"
+    }
+  });
+  $('.gallery__like-btn').on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('gallery__like-btn--clicked');
+  });
 }); // Your functions here
